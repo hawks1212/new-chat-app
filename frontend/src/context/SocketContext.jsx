28c7f058e106508b023326ba48cb68b09f7de442
simon <simonkatich0037@gmail.com>
2024-12-09 00:44:28 +0530

    Deploed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-app-j4k9.onrender.com", {
+      const socket = io("https://newchat-app.onrender.com", {
         query: {
           userId: authUser._id,
         },
